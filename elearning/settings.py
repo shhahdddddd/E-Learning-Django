8451cb1b589340343d32dac39abf9809718e0597
shahd ben hmida <152545736+shhahdddddd@users.noreply.github.com>
2025-05-18 23:29:24 +0100

    settings.py

--- elearning/settings.py
+++ elearning/settings.py
@@ -81,13 +81,14 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASSWORD'],
-            'HOST': os.environ['DB_HOST'],
-            'PORT': os.environ['DB_PORT'],
+            'NAME': os.environ.get('DB_NAME', 'mydefaultdb'),
+            'USER': os.environ.get('DB_USER', 'mydefaultuser'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+            'HOST': os.environ.get('DB_HOST', 'localhost'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
         }
     }
+
 
 # ===== AUTHENTICATION =====
 AUTH_PASSWORD_VALIDATORS = [
